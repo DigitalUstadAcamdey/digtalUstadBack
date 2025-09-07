968d2a942169067fd7346c817586f5dbf4a69979
imadkasse <kasseimad81@gmail.com>
2025-09-07 10:58:02 +0100

    fixing errors

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -108,8 +108,9 @@ exports.signup = catchAsync(async (req, res, next) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production" ? true : false,
-    sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
     maxAge: 24 * 60 * 60 * 1000,
+    domain: ".onrender.com",
     path: "/",
   });
   res.status(200).json({
